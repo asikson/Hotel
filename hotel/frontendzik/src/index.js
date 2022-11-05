a5e23c7c22a38e764704f7406d0e66f36d8dec92
Monika Leszniewska <monia.leszniewska@gmail.com>
2022-11-05 18:49:32 +0100

    Bug fix ReactDOM.render is no longer supported in React 18

--- hotel/frontendzik/src/index.js
+++ hotel/frontendzik/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
